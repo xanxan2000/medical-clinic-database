055dba597136ed952a5da9f86a8fe23b0b61ef35
xanxan2000 <98865289+xanxan2000@users.noreply.github.com>
2022-02-28 12:33:18 -0600

    Update medicalClinicSQL.sql

--- medicalClinicSQL.sql
+++ medicalClinicSQL.sql
@@ -31,7 +31,47 @@ CREATE TABLE patient(
     address VARCHAR(30),
     city VARCHAR(10),
     state VARCHAR(10),
-    zipcode VARCHAR(10)
+    patient_id CHAR(10) NOT NULL,
+    zipcode VARCHAR(10),
+    PRIMARY KEY(patient_id)
+);
+
+
+DROP TABLE office;
+
+CREATE TABLE office(
+	office_id CHAR(10) NOT NULL,
+	office_name VARCHAR(50),
+	address VARCHAR(50),
+	office_phone CHAR(11),
+	PRIMARY KEY(office_id)
+);
+
+DROP TABLE room;
+
+CREATE TABLE room(
+	room_num SMALLINT,
+	unavaliable BOOLEAN
+);
+
+DROP TABLE appointment;
+
+CREATE TABLE appointment(
+    appt_id CHAR(10) NOT NULL,
+    patient_id CHAR(10) NOT NULL,
+    nurse_id CHAR(10),
+    doctor_id CHAR(10) NOT NULL,
+    start DATETIME,
+    end DATETIME,
+    office_id CHAR(10) NOT NULL,
+    room_num SMALLINT,
+    reason_appt VARCHAR(100),
+    PRIMARY KEY(appt_id),
+    FOREIGN KEY(patient_id) REFERENCES patient(patient_id),
+    FOREIGN KEY(nurse_id) REFERENCES clinic_employee(employee_id),
+    FOREIGN KEY(doctor_id) REFERENCES clinic_employee(employee_id),
+    FOREIGN KEY(office_id) REFERENCES office(office_id),
+    FOREIGN KEY(room_num) REFERENCES room(room_num),
 );
 
 INSERT INTO patient VALUE ("Malik", "Taylor");
