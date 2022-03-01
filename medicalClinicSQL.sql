d3dfef1788dd0113cc73671d80162aeb831a8219
Xander <98865289+xanxan2000@users.noreply.github.com>
2022-03-01 15:19:14 -0600

    Update medicalClinicSQL.sql

--- medicalClinicSQL.sql
+++ medicalClinicSQL.sql
@@ -31,7 +31,43 @@ CREATE TABLE patient(
     address VARCHAR(30),
     city VARCHAR(10),
     state VARCHAR(10),
-    zipcode VARCHAR(10)
+    zipcode VARCHAR(10),
+    patient_id INT NOT NULL PRIMARY KEY
+
+);
+
+
+
+DROP TABLE specialty;
+
+CREATE TABLE specialty(
+	specialty_id SMALLINT NOT NULL PRIMARY KEY,
+	specialty_name VARCHAR(20) NOT NULL
+	);
+	
+
+DROP TABLE doctor;
+
+CREATE TABLE doctor( 
+    f_name VARCHAR(20) NOT NULL,
+    l_name VARCHAR(20) NOT NULL,
+    employee_id CHAR(10) NOT NULL,
+    birth_date DATE,
+    race VARCHAR(20),
+    ethnicity VARCHAR(20),
+    sex VARCHAR(10),
+    email VARCHAR(30),
+    phone_number VARCHAR(15),
+    address VARCHAR(30),
+    city VARCHAR(10),
+    state VARCHAR(10),
+    zipcode VARCHAR(10),
+    hired DATETIME NOT NULL,
+    specialty_id CHAR(5) NOT NULL,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (employee_id) REFERENCES clinic_employee(employee_id),
+    FOREIGN KEY (specialty_id) REFERENCES specialty (specialty_id)     
+
 );
 
 INSERT INTO patient VALUE ("Malik", "Taylor");
